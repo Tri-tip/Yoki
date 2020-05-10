873e8bc5a0ad08228ff66c9c3aede3db1dce9bbb
Zaid Arshad <mahmooa62@gmail.com>
2020-05-10 03:22:37 -0400

    recursive load

--- internal_functions/load_commands.js
+++ internal_functions/load_commands.js
@@ -2,16 +2,21 @@ const fs = require('fs');
 
 
 module.exports.load_commands = (client) => {
-    const commandFiles = fs.readdirSync(__dirname + '../commands');
+    readDirectoryRecursive(client, 'commands')
 }
 
-let readDirectoryRecursive = (dir) => {
-    for(file of commandFiles) {
-        if(fs.lstatSync(__dirname + "../commands/" + file).isDirectory()) {
-            readDirectoryRecursive();
-        } else {
-            let tempreq = require('../commands/' + file);
-            client.commands.bot_commands.set(tempreq.name, tempreq)
-        }
-    }
+let readDirectoryRecursive = (client, source, path = "\commands") => {
+    fs.readdirSync(process.cwd() + "\\" + source, {
+            withFileTypes: true
+        })
+        .forEach(file => {
+            if (file.isDirectory()) {
+                path += '\\' + file.name
+                readDirectoryRecursive(client, path, path);
+            } else {
+                let tempreq = require('../' + path + "\\" + file.name);
+                return client.commands.bot_commands.set(tempreq.name, tempreq)
+            }
+            path = "\commands";
+        })
 }
